7f48ddefc253d10693571b6d07d3b0f2ea5e6003
markcus <markcus0526@proton.me>
2026-03-06 04:33:48 +0100

    Add proxy-client module 3

---
+++ proxy-client/src/main/java/com/markcus/spring/business/user/service/VerificationTokenClientService.java
@@ -0,0 +1,67 @@
+package com.markcus.spring.business.user.service;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.markcus.spring.business.user.model.VerificationTokenDto;
+import com.markcus.spring.business.user.model.response.VerificationUserTokenServiceCollectionDtoResponse;
+
+@FeignClient(
+		name = "USER-SERVICE",
+		contextId = "verificationTokenClientService",
+		path = "/user-service/api/verificationTokens",
+		dismiss404 = true)
+public interface VerificationTokenClientService {
+	
+	@GetMapping
+	ResponseEntity<VerificationUserTokenServiceCollectionDtoResponse> findAll();
+	
+	@GetMapping("/{verificationTokenId}")
+	ResponseEntity<VerificationTokenDto> findById(
+			@PathVariable("verificationTokenId") 
+			@NotBlank(message = "*Input must not blank!**")
+			@Valid final String verificationTokenId);
+	
+	@PostMapping
+	ResponseEntity<VerificationTokenDto> save(
+			@RequestBody 
+			@NotNull(message = "*Input must not NULL!**")
+			@Valid final VerificationTokenDto verificationTokenDto);
+	
+	@PutMapping
+	ResponseEntity<VerificationTokenDto> update(
+			@RequestBody 
+			@NotNull(message = "*Input must not NULL!**") 
+			@Valid final VerificationTokenDto verificationTokenDto);
+	
+	@PutMapping("/{verificationTokenId}")
+	ResponseEntity<VerificationTokenDto> update(
+			@PathVariable("verificationTokenId") 
+			@NotBlank(message = "*Input must not blank!**") final String verificationTokenId, 
+			@RequestBody 
+			@NotNull(message = "*Input must not NULL!**") 
+			@Valid final VerificationTokenDto verificationTokenDto);
+	
+	@DeleteMapping("/{verificationTokenId}")
+	ResponseEntity<Boolean> deleteById(@PathVariable("verificationTokenId") @NotBlank(message = "*Input must not blank!**") @Valid final String verificationTokenId);
+	
+}
+
+
+
+
+
+
+
+
+
+
---
+++ proxy-client/src/test/java/com/markcus/spring/ProxyClientApplicationTests.java
@@ -0,0 +1,10 @@
+package com.markcus.spring;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyClientApplicationTests {
+	
+	
+	
+}
